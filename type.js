226ff7f632ecea7f6581de3f0ef9f0c0b0dca72f
dmitriy ryabinkin <tom147@list.ru>
2021-09-27 20:40:09 +0300

    1st

---
+++ type.js
@@ -0,0 +1,13 @@
+let date;
+date = 24;
+let planetEarth=true;
+let life="long long blablabla";
+let big=1234567890n;
+let age=null;
+let hz;
+typeof date;
+typeof planetEarth;
+typeof life;
+typeof big;
+typeof age;
+typeof hz;
