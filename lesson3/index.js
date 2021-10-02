e0db21c5500606631074e8d17f7cc02e6d8b9861
Dmitriy Ryabinkin <tom147@list.ru>
2021-10-02 11:16:18 +0300

    1st

---
+++ lesson3/index.js
@@ -0,0 +1,64 @@
+// const firstValue = String(5);
+
+// console.log(typeof firstValue);
+
+// const obj = {
+//   foo: "bar",
+// };
+
+// console.log(typeof String(obj.foo));
+
+// let testValue = "12345";
+
+// console.log(typeof testValue);
+
+// testValue = Number(testValue);
+
+// console.log(testValue);
+
+// const isUser = true;
+// const isAdmin = false;
+
+// console.log(Nuber(isUser), Number(isAdmin));
+
+// const voidString = "";
+
+// console.log(Number(voidString));
+
+// const value = "";
+
+// console.log(typeof Boolean(value), Boolean(value));
+
+// const obj = [1].length;
+// console.log(Boolean(obj));
+
+// console.log(4 % 2); //остаток от деления
+
+//  Унарный плюс
+
+// const value = "5";
+// console.log(typeof value, typeof +value, typeof Number(value));
+
+//Бинарный плюс
+// let sum = 5 + 6;
+// console.log(sum);
+
+// sum = 1 + true;
+// console.log(sum);
+
+// sum = 5 + null;
+// console.log(sum);
+
+// const stringValue = "12";
+// const numberValue = 34;
+
+// console.log(+stringValue + numberValue);
+// console.log(typeof (numberValue + +stringValue));
+
+const firstName = "Alex";
+// coio
+const lastName = "ivanov";
+
+console.log(firstName + " " + lastName);
+
+console.log(`${firstName} ${lastName}`);
