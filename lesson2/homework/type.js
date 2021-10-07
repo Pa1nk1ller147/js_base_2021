403d7ea43d39ce4f7a5e35d77755e51ee558b0b5
Dmitriy Ryabinkin <tom147@list.ru>
2021-10-07 13:22:54 +0300

    1

---
+++ lesson2/homework/type.js
@@ -0,0 +1,17 @@
+let date;
+date = 24;
+let planetEarth=true;
+let life="long long blablabla";
+let big=1234567890n;
+let age=null;
+let hz;
+typeof date;
+typeof planetEarth;
+typeof life;
+typeof big;
+typeof age;
+typeof hz;
+
+const planet = "Earth";
+planet = "Moon";
+ 
