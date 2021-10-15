db85d8a7d9b74946dc2ccbd2b2ee2dace6209ca3
Dmitriy Ryabinkin <tom147@list.ru>
2021-10-15 17:41:35 +0300

    15.10

---
+++ lesson4/classwork/index.js
@@ -0,0 +1,144 @@
+/* if(true) {
+    console.log('true')
+}
+if(false){
+    console.log('false')
+}
+
+
+console.log('start');
+
+const value = -5;
+if(value >0){
+    console.log('true');
+}
+else{
+    console.log('false');
+}
+console.log('finish'); */
+
+/* console.log('start'); 
+
+const value = 1;
+
+if(value === 0) {
+    console.log(0, ' = value')
+} else if (value > 1) {
+    console.log(value > '> 1');
+}else if (value < -1) {
+    console.log(value, '< -1');
+} else { console.log(value);}
+
+console.log('finish'); */
+
+
+/* const age = 10;
+const result = age >=18 ? 'Доступ разрешен' : 'Запрещен'
+console.log(result); */
+
+
+/* 
+switch ('Alex') {
+
+    case ' Ivan':
+        console.log('Ivan');
+        break
+    case 'Alex':
+        console.log('Лёша, человек');
+        break
+    case 'Max':
+        console.log('Max')
+        break
+    default:
+        console.log('Имени нет в списке')        
+} */
+
+
+/* const temp= 25;
+if(temp < -30) {
+    console.log(-30, ' = Оставайтесь дома!')
+} else if (temp >= -30) {
+    console.log('Сегодня холодно');
+} else if (temp <= -10) {
+    console.log('Сегодня холодно');
+} else if (temp >= -10) {
+    console.log('Не холодно');
+} else if (temp <= 5) {
+    console.log('Не холодно');
+} else if (temp >= 5) {
+    console.log('Тепло');
+} else if (temp <= 15) {
+    console.log('Тепло');
+} else if (temp >= 15) {
+    console.log('Очень тепло');
+} else if (temp <= 25) {
+    console.log('Очень тепло');
+}else if (temp >= 25) {
+    console.log('Жарко');
+} else if (temp >= 35) {
+    console.log('Пекло');
+}    
+ */
+
+/* 
+switch (Alex) {
+    case 'Ivan';
+    console.log("Ваня, 25 лет")
+    break
+} */
+
+
+
+
+
+
+
+
+/* const a = 5;
+const b = 2;
+const c = 3;
+
+if(a > b){
+    a>c ? console.log(a) : console.log(c);
+}else{
+    b>c ? console.log(a) : console.log(c);
+} */
+
+switch ("Alex") {
+    case 'Ivan':
+        console.log('Ваня, 25 лет');
+        break;
+    case 'Alex':
+        console.log('Алёша, студент');
+        break;
+  default:
+        console.log('Имени нет в списке')
+        break;
+}
+
+
+
+/* const a = 0;
+const b = 1;
+const result1 = a && b; // можно вместо && поставить / тогда сначала выдет true, а сейчас false
+console.log(result1); */
+
+/* const a = 1;
+const b = 2;
+const c = 3;
+
+if(a>b && a>c) {
+    console.log(a);
+}else if(b>a && b>c){
+    console.log(b);
+}
+ */
+
+/* /* 
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i += 1;
+} 
+console.log('end') */
+
---
+++ lesson4/homework/index.js
@@ -0,0 +1,158 @@
+/*  */
+
+// let a = 5;
+// let b = 15;
+// let c = 3;
+
+// if (a > b && a > c) {
+//   console.log(a);
+// } else if (a < b && a > c) {
+//   console.log(b);
+// } else if (a < b && b < c) {
+//   console.log(c);
+// }
+
+// const temp = 4;
+// if (temp < -30) {
+//   console.log(-30, " = Оставайтесь дома!");
+// } else if (temp <= -10) {
+//   console.log("Сегодня холодно");
+// } else if (temp <= 5) {
+//   console.log("Не холодно");
+// } else if (temp <= 15) {
+//   console.log("Очень тепло");
+// } else if (temp <= 25) {
+//   console.log("Жарко");
+// } else if (temp >= 35) {
+//   console.log("Пекло");
+// }
+
+// switch ("user") {
+//   case "admin":
+//     console.log("самый главный");
+//     break;
+//   case "manager":
+//     console.log("не такй главный");
+//     break;
+//   case "user":
+//     console.log("ну такое");
+//     break;
+// }
+
+// let year = 1;
+// let n = 5
+// for(;year <= n; year++){
+// console.log("string")
+// }
+
+// let m=100;                   //автомобили
+// let n=23;
+// let days=0;
+// while (m>=n){
+//   m/=2;
+//   days+=1;
+// }
+// console.log(days)
+
+// let m=1800;       //високосные года
+// let n=2000;
+// for(m;m<=n;m=m+4){
+//   console.log(m);
+//   console.log(iters)
+
+// }
+// let m=1800; 
+// let n=2000;
+// let iters=0; 
+// while(m<=n){
+//   m+=4;
+//   iters+=1;
+// }
+// console.log(iters-1);
+// console.log(m-4)
+
+
+
+/*                 Работа с if-else              */
+// let a = -3;
+// if (a === 0){
+//   console.log('Верно')
+// } else if(a != 0){
+//   console.log('Неверно')
+// } 
+
+// let a = -1;
+// if (a > 0) {
+//     console.log('Верно')
+//   } else if(a >= 0){
+//     console.log('Неверно')
+//   } else if(a <= 0){
+//     console.log('Неверно')
+//   }
+
+// let a = 0;
+// if (a < 0) {
+//     console.log('Верно')
+//   } else if(a >= 0){
+//     console.log('Неверно')
+//   } else if(a <= 0){
+//     console.log('Неверно')
+//   }
+
+// let a = -1;
+// if (a >= 0) {
+//     console.log('Верно')
+//   } else if(a < 0){
+//     console.log('Неверно')
+//   }
+
+// let a = -1;
+// if (a <= 0) {
+//     console.log('Верно')
+//   } else if(a > 0){
+//     console.log('Неверно')
+//   }
+
+// let a = 0;
+// if (a !== 0) {
+//     console.log('Верно')
+//   } else if(a === 0) {
+//     console.log('Неверно')
+//   }
+
+// let a = "test";
+// if (a === "test") {
+//     console.log('Верно')
+//   } else {
+//     console.log('Неверно')
+//   }
+
+// let a = '2';
+// if (a == 1) {
+//     console.log('Верно')
+//   } else {
+//     console.log('Неверно')
+//   }
+
+
+/*              Работа с логическими переменными                 */
+// let a = true;
+// if (a == true){
+//   console.log('Верно')
+// } else {
+// console.log('Неверно')
+// }
+
+// let a = ''
+// a == true ? console.log('Верно') : console.log('Неверно')
+
+
+// let a = 0;
+// if (a != true){
+//   console.log('Верно')
+// } else {
+// console.log('Неверно')
+// }
+
+// let a = 1
+// a != true ? console.log('Верно') : console.log('Неверно')
